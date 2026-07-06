0b80382aef6dc0c6fa64664a50fbb2351dcb8fa0
evanskyeiakora-lab <evanskyeiakora@gmail.com>
2026-07-06 16:07:50 +0000

    Sprint 1 - Authentication

--- config.py
+++ config.py
@@ -12,3 +12,11 @@ class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "acg.db")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+UPLOAD_FOLDER = "app/static/uploads"
+NEWS_UPLOAD_FOLDER = "app/static/uploads/news"
+GALLERY_UPLOAD_FOLDER = "app/static/uploads/gallery"
+SLIDER_UPLOAD_FOLDER = "app/static/uploads/slides"
+
+MAX_CONTENT_LENGTH = 5 * 1024 * 1024  # 5 MB
+ALLOWED_EXTENSIONS = {"jpg", "jpeg", "png", "webp"}
