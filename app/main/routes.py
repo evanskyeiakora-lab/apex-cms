54e959d1e0ef4f456acff408ba5c2e8fc57c3833
evanskyeiakora-lab <evanskyeiakora@gmail.com>
2026-07-15 17:52:21 +0000

    Initial Apex CMS

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,21 @@
 from flask import render_template
 
 from . import main_bp
+from app.models import News
 
 
 @main_bp.route("/")
 def home():
-    return render_template("index.html")
+
+    latest_news = (
+        News.query
+        .filter_by(status="published")
+        .order_by(News.published_at.desc())
+        .limit(3)
+        .all()
+    )
+
+    return render_template(
+        "index.html",
+        latest_news=latest_news
+    )
